6bb2da78c0584c5e17adf9098c9cb5cbac9192b9
Ben Halterman <ben.halterman@gmail.com>
2013-04-23 13:25:23 -0400

    playing with opengl context

--- CatPixx/AppDelegate.h
+++ CatPixx/AppDelegate.h
@@ -12,4 +12,6 @@
 
 @property (assign) IBOutlet NSWindow *window;
 
++ (NSOpenGLPixelFormat *)getPixelFormat:(CGDirectDisplayID)displayID;
+
 @end
--- CatPixx/AppDelegate.m
+++ CatPixx/AppDelegate.m
@@ -8,12 +8,34 @@
 
 #import "AppDelegate.h"
 #import "Display.h"
+#include <OpenGL/gl.h>
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
     NSArray *displays = [Display getCurrentDisplayList];
+    CGDirectDisplayID displayID = [[displays objectAtIndex:0] displayID];
+    NSOpenGLPixelFormat *pixelFormat = [AppDelegate getPixelFormat:displayID];
+    NSOpenGLContext *context = (NSOpenGLContext *)[[NSOpenGLContext alloc] initWithFormat:pixelFormat shareContext:nil];
+    [context makeCurrentContext];
+    [context setFullScreen];
+    
+    glColor3f(1.0f, 0.85f, 0.35f);
+    glBegin(GL_TRIANGLES);
+    {
+        glVertex3f(  0.0,  0.6, 0.0);
+        glVertex3f( -0.2, -0.3, 0.0);
+        glVertex3f(  0.2, -0.3 ,0.0);
+    }
+    glEnd();
+}
+
++ (NSOpenGLPixelFormat *)getPixelFormat:(CGDirectDisplayID)displayID {
+    NSOpenGLPixelFormatAttribute attributes [] = {
+        (NSOpenGLPixelFormatAttribute) CGDisplayIDToOpenGLDisplayMask(displayID),
+        NSOpenGLPFADoubleBuffer,
+        (NSOpenGLPixelFormatAttribute)nil };
+    return (NSOpenGLPixelFormat *)[[NSOpenGLPixelFormat alloc] initWithAttributes:attributes];
 }
 
 @end
